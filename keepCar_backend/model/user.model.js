b3346fddd745827580caf672acbe4cad61449eb0
Chameen2001 <dilhara1117@gmail.com>
2022-10-05 20:00:57 +0530

    create user model

--- keepCar_backend/model/user.model.js
+++ keepCar_backend/model/user.model.js
@@ -7,4 +7,22 @@ const userSchema = new Schema({
     required: true,
     trim: true,
   },
+  surName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    require: true,
+    trim: true,
+  },
 });
+
+const user = mongoose.model("user", userSchema);
+module.exports = user;
